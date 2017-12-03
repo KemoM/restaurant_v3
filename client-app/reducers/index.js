ab04b3a897c1b7c8a89b445761d71938f3d77905
kemal.muhovic@gmail.com <kemal.muhovic@gmail.com>
2017-12-03 16:56:08 +0100

    Removing unused reducer

--- client-app/reducers/index.js
+++ client-app/reducers/index.js
@@ -3,13 +3,11 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import { authentication } from './authentication.reducer';
 import { registration } from './registration.reducer';
-import { users } from './users.reducer';
 
 const rootReducer = combineReducers({
   routing: routerReducer,
   authentication: authentication,
   registration : registration,
-  users: users,
   form: formReducer,
 });
 
