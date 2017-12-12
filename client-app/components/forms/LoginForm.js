dca95105f5550316bdb7e2e671f196ea97931c23
kemal.muhovic@gmail.com <kemal.muhovic@gmail.com>
2017-12-12 17:45:44 +0100

    Creating LoginForm and injecting it into LoginPage container

---
+++ client-app/components/forms/LoginForm.js
@@ -0,0 +1,74 @@
+﻿import React from 'react';
+import { connect } from 'react-redux';
+import { Field, reduxForm } from 'redux-form';  
+import { Link } from 'react-router-dom';
+import {
+  Row,
+  Col,
+  Button,
+  Card,
+  CardHeader,
+  CardFooter,
+  CardBody,
+  Form,
+} from 'reactstrap';
+
+import { loginValidation } from './validators/loginValidation';
+import { renderInputField } from './renderInputField';
+
+class LoginForm extends React.Component {
+  renderAlert() {
+    if (this.props.errorMessage) {
+      return (<div><span><strong>Error!</strong> {this.props.errorMessage}</span></div>);
+    }
+  }
+
+  render() {
+    const { fields: {username, password}, loggingIn, pristine, handleSubmit } = this.props;
+    return (
+      <div className="flex-row align-items-center">
+        <Row>
+          <Col md="6">
+            <Card>
+              <CardHeader>
+                <strong>Login</strong> Form
+              </CardHeader>
+              <CardBody>
+                <Form onSubmit={handleSubmit} >
+                  <Field type="text" id="username" name="username" label="Username" labelClass="pr-1" component={renderInputField} value={username} />
+                  <Field type="password" id="password" name="password" label="Password" component={renderInputField} value={password} />
+                  {this.renderAlert()}
+                  <Button type="submit" disabled={pristine || loggingIn} size="sm" color="primary"><i className="fa fa-dot-circle-o" /> Login</Button>
+                  {loggingIn && 
+                    <img alt="" src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
+                  }
+                </Form>
+              </CardBody>
+              <CardFooter>
+                <span>Or you want to register: </span>
+                <Link to="/register" className="btn btn-link">Register</Link>
+              </CardFooter>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  const loggingIn = state.authentication.get('loggingIn');
+  const errorMessage = state.authentication.get('error');
+  return {
+    loggingIn,
+    errorMessage,
+  };
+}
+
+const formConfiguration = {  
+  form: 'loginform',
+  fields: ['username', 'password'],
+  validate: loginValidation,
+};
+
+export default connect(mapStateToProps)(reduxForm(formConfiguration)(LoginForm));
--- client-app/containers/LoginPage.js
+++ client-app/containers/LoginPage.js
@@ -1,22 +1,7 @@
 ﻿import React from 'react';
-import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { Field, reduxForm } from 'redux-form';  
-import {
-  Row,
-  Col,
-  Button,
-  Card,
-  CardHeader,
-  CardFooter,
-  CardBody,
-  Form,
-} from 'reactstrap';
-
+import LoginForm from '../components/forms/LoginForm';
 import { userActions } from '../actions/userActions';
-import { loginValidation } from '../components/forms/validators/loginValidation';
-import { renderInputField } from '../components/forms/renderInputField';
-
 
 class LoginPage extends React.Component {
   constructor(props) {
@@ -30,76 +15,26 @@ class LoginPage extends React.Component {
       password: '',
     };
 
-    this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(e) {
-    const { name, value } = e.target;
-    this.setState({ [name]: value });
-  }
-
   handleSubmit(e) {
-    e.preventDefault();
+    // No "preventDefault" because Redux Form does it.
 
-    const { username, password } = this.state;
-    const { dispatch, valid } = this.props;
-    if (valid) {
-      dispatch(userActions.login(username, password));
-    }
+    const { username, password } = e;
+    const { dispatch } = this.props;
+    dispatch(userActions.login(username, password));
   }
   
-  renderAlert() {
-    if (this.props.errorMessage) {
-      return (<div><span><strong>Error!</strong> {this.props.errorMessage}</span></div>);
-    }
-  }
-
-  //TODO - Create Login component
   render() {
-    const { loggingIn, pristine } = this.props; //eslint-disable-line
-    const { username, password } = this.state;
-
     return (
       <div className="flex-row align-items-center">
-        <Row>
-          <Col md="6">
-            <Card>
-              <CardHeader>
-                <strong>Login</strong> Form
-              </CardHeader>
-              <CardBody>
-                <Form onSubmit={this.handleSubmit} >
-                  <Field type="text" id="username" name="username" label="Username" labelClass="pr-1" component={renderInputField} value={username} onChange={this.handleChange} />
-                  <Field type="password" id="password" name="password" label="Password" component={renderInputField} value={password} onChange={this.handleChange} />
-                  {this.renderAlert()}
-                  <Button type="submit" disabled={pristine || loggingIn} size="sm" color="primary"><i className="fa fa-dot-circle-o" /> Login</Button>
-                </Form>
-              </CardBody>
-              <CardFooter>
-                <span>Or you want to register: </span>
-                <Link to="/register" className="btn btn-link">Register</Link>
-              </CardFooter>
-            </Card>
-          </Col>
-        </Row>
+        <LoginForm
+          onSubmit={this.handleSubmit}
+        />
       </div>
     );
   }
 }
 
-function mapStateToProps(state) {
-  const loggingIn = state.authentication.get('loggingIn'); //should be used for loader gif, TODO
-  const errorMessage = state.authentication.get('error');
-  return {
-    loggingIn,
-    errorMessage,
-  };
-}
-
-const formConfiguration = {  
-  form: 'login',
-  validate: loginValidation,
-};
-
-export default connect(mapStateToProps)(reduxForm(formConfiguration)(LoginPage));
+export default connect()(LoginPage);
